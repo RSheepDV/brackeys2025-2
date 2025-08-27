41a3112a39ca806f276d391a34ff967ce418aacc
Praket <praketaggarwal@gmail.com>
2025-08-26 22:07:12 -0700

    deleted the unused world.gd script

--- Racoons/world.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-@onready var mouse: CharacterBody3D = $Mouse
-
-func _physics_process(delta: float) -> void:
-	get_tree().call_group("Enemies","update_target_location",mouse.global_transform.origin)
